80c84a294cf83660bf4d05402c086eab511f76d0
Misha <creatorlogic1@gmail.com>
2021-03-28 23:38:23 +0300

    added basic structure

---
+++ Rectangle.Console/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Rectangle.Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
